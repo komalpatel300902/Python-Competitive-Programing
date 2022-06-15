e5a1ad6699f72885a7d0fa7b8a3de5a89d6262a4
komal patel <komal14march2002@gmail.com>
2022-06-15 14:30:29 +0530

    Completed text wrap Question

---
+++ src/problem_with_solution_text_wrap.py
@@ -0,0 +1,15 @@
+"""Wraping the text in Perticural width."""
+
+import textwrap
+
+def wrap(string, max_width):
+    str = ''
+    for x in range(0,len(string)):
+        if x != 0 and x%(max_width) == 0:
+            str += "\n"
+        str += string[x] 
+    return str
+
+string, max_width = input(), int(input())
+result = wrap(string, max_width)
+print(result)
