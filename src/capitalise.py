6baaf0499bcffa9785ecf0c2b9b3d97e308c7eb8
komal patel <komal14march2002@gmail.com>
2022-06-18 11:00:45 +0530

    did some changes in capitalize

--- src/capitalise.py
+++ src/capitalise.py
@@ -8,12 +8,7 @@ def solve(s):
     return " ".join(refined_words)
 
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
+   
     s = input()
-
     result = solve(s)
-
-    fptr.write(result + '\n')
-
-    fptr.close()
+    print(result)
